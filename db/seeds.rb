4ca4f5c04b448c747ecc9e1071032ba7b8025575
Daniel Amarilho <dcamarilho@gmail.com>
2019-05-30 16:48:26 -0300

    Add seed with pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -98,15 +98,17 @@ flat_attributes = [
     planet:       'Earth',
     address:      '#345-Z5G5',
     price:        7000,
-    user_id:      '8'
-  },
+    user_id:      '8',
+    remote_photo_url:        'https://res.cloudinary.com/dxzy7a44p/image/upload/v1559236238/SEArch_-NightTime_lores_jisfhg.jpg'
+},
   {
     name:         'Queen Size - Artistic Flat - Amazing View',
     description:  'Another journey chamber way yet females man. Way extensive and dejection get delivered deficient sincerity gentleman age. Too end instrument possession contrasted motionless. Calling offence six joy feeling. Coming merits and was talent enough far. Sir joy northward sportsmen education. Discovery incommode earnestly no he commanded if. Put still any about manor heard.',
     planet:       'Mars',
     address:      '#345-Z5G5',
     price:        5000,
-    user_id:      '6'
+    user_id:      '6',
+    remote_photo_url:        'https://res.cloudinary.com/dxzy7a44p/image/upload/v1559236237/SEArch_-Wardroom_lores_nhsrdh.jpg'
   },
   {
     name:         'Bæjarins Beztu Pylsur King Size Bed',
@@ -114,7 +116,8 @@ flat_attributes = [
     planet:       'Moon',
     address:      '#345-Z5G5',
     price:        5600,
-    user_id:      '10'
+    user_id:      '10',
+    remote_photo_url:        'https://res.cloudinary.com/dxzy7a44p/image/upload/v1559236237/Marsha-3d-printed-home-for-mars-ai-space-factory-8_u6qmqn.jpg'
   },
   {
     name:         'The Muse Haus I - Black Sheep',
@@ -122,7 +125,8 @@ flat_attributes = [
     planet:       'Ceres',
     address:      '#345-Z5G5',
     price:         7200,
-    user_id:      '5'
+    user_id:      '5',
+    remote_photo_url:        'https://res.cloudinary.com/dxzy7a44p/image/upload/v1559236237/SEArch_-Construction-2_lores_gb0ivq.jpg'
   },
   {
     name:         'Queen Size - Artistic Flat - Amazing View',
@@ -130,7 +134,8 @@ flat_attributes = [
     planet:       'Mars',
     address:      '#345-Z5G5',
     price:        4250,
-    user_id:      '2'
+    user_id:      '2',
+    remote_photo_url:        'https://res.cloudinary.com/dxzy7a44p/image/upload/v1559236237/SEArch_-CrouchingSpaceMan_lores__1_kcdhh0.jpg'
   },
   {
     name:         'The Muse Haus II - Black Sheep',
@@ -138,7 +143,8 @@ flat_attributes = [
     planet:       'Venus',
     address:      '#345-Z5G5',
     price:        9600,
-    user_id:      '7'
+    user_id:      '7',
+    remote_photo_url:        'https://res.cloudinary.com/dxzy7a44p/image/upload/v1559236237/SEArch_-Bedroom_Outside_lores_d4ex03.jpg'
   },
   {
     name:         'Bæjarins Beztu Pylsur',
@@ -146,7 +152,8 @@ flat_attributes = [
     planet:       'Earth',
     address:      '#345-Z5G5',
     price:        7000,
-    user_id:      '1'
+    user_id:      '1',
+    remote_photo_url:        'https://res.cloudinary.com/dxzy7a44p/image/upload/v1559236237/Marsha-3d-printed-home-for-mars-ai-space-factory-6_cd2evh.jpg'
   },
   {
     name:         'Artistic Structure - Amazing View',
@@ -154,7 +161,8 @@ flat_attributes = [
     planet:       'Mars',
     address:      '#345-Z5G5',
     price:        5000,
-    user_id:      '6'
+    user_id:      '6',
+    remote_photo_url:        'https://res.cloudinary.com/dxzy7a44p/image/upload/v1559236236/mars-rover-panorama-murray-buttes-PIA20765-2_htrl2h.jpg'
   },
   {
     name:         'Stroll to Harbor from a Cozy Apartment',
@@ -162,7 +170,8 @@ flat_attributes = [
     planet:       'Ceres',
     address:      '#345-Z5G5',
     price:        5600,
-    user_id:      '10'
+    user_id:      '10',
+    remote_photo_url:        'https://res.cloudinary.com/dxzy7a44p/image/upload/v1559236236/Marsha-3d-printed-home-for-mars-ai-space-factory-2-1024x563_vgda6a.jpg'
   },
   {
     name:         'The Muse Haus I - Bæjarins Beztu Pylsur',
@@ -170,7 +179,8 @@ flat_attributes = [
     planet:       'Ceres',
     address:      '#345-Z5G5',
     price:        7200,
-    user_id:      '5'
+    user_id:      '5',
+    remote_photo_url:        'https://res.cloudinary.com/dxzy7a44p/image/upload/v1559236236/Mars-Ice-House_section_lr_c9zpj1.jpg'
   },
   {
     name:         'Golden Circle Glaciar',
@@ -178,7 +188,8 @@ flat_attributes = [
     planet:       'Mars',
     address:      '#345-Z5G5',
     price:        4250,
-    user_id:      '3'
+    user_id:      '3',
+    remote_photo_url:        'https://res.cloudinary.com/dxzy7a44p/image/upload/v1559236236/Marsha-3d-printed-home-for-mars-ai-space-factory-5-1024x578_hitjp7.jpg'
   },
   {
     name:         'Stroll to Harbor from a Cozy House',
@@ -186,7 +197,8 @@ flat_attributes = [
     planet:       'Venus',
     address:      '#345-Z5G5',
     price:        9600,
-    user_id:      '7'
+    user_id:      '7',
+    remote_photo_url:        'https://res.cloudinary.com/dxzy7a44p/image/upload/v1559236236/Marsha-3d-printed-home-for-mars-ai-space-factory-4-1024x512_d0ctju.jpg'
   },
   {
     name:         'King Size Bed - Artistic Flat - Amazing View',
@@ -194,7 +206,8 @@ flat_attributes = [
     planet:       'Earth',
     address:      '#345-Z5G5',
     price:        7000,
-    user_id:      '8'
+    user_id:      '8',
+    remote_photo_url:        'https://res.cloudinary.com/dxzy7a44p/image/upload/v1559236236/Marsha-3d-printed-home-for-mars-ai-space-factory-3-1024x512_sqa7nm.jpg'
   },
   {
     name:         'Queen Size - Artistic Flat - Amazing View',
@@ -202,7 +215,8 @@ flat_attributes = [
     planet:       'Mars',
     address:      '#345-Z5G5',
     price:        5000,
-    user_id:      '6'
+    user_id:      '6',
+    remote_photo_url:        'https://res.cloudinary.com/dxzy7a44p/image/upload/v1559236235/Mars-Ice-House_Dusk-02_lr_baibsa.jpg'
   },
   {
     name:         'Bæjarins Beztu Pylsur King Size Bed',
@@ -210,7 +224,8 @@ flat_attributes = [
     planet:       'Moon',
     address:      '#345-Z5G5',
     price:        5600,
-    user_id:      '10'
+    user_id:      '10',
+    remote_photo_url:        'https://res.cloudinary.com/dxzy7a44p/image/upload/v1559236183/Clouds-AO-and-SEArch-wins-NASA-Mars-Habitat-contest-_dezeen_1568_fbwvaz.jpg'
   },
   {
     name:         'The Muse Haus I - Black Sheep',
@@ -218,7 +233,8 @@ flat_attributes = [
     planet:       'Ceres',
     address:      '#345-Z5G5',
     price:        7200,
-    user_id:      '1'
+    user_id:      '1',
+    remote_photo_url:        'https://res.cloudinary.com/dxzy7a44p/image/upload/v1559242983/138008main_jsc2005e43267_hi_ui3xap.jpg'
   },
   {
     name:         'Queen Size - Artistic Flat - Amazing View',
@@ -226,7 +242,8 @@ flat_attributes = [
     planet:       'Mars',
     address:      '#345-Z5G5',
     price:        4250,
-    user_id:      '10'
+    user_id:      '10',
+    remote_photo_url:        'https://res.cloudinary.com/dxzy7a44p/image/upload/v1559242983/Bubble-milky-wayGuildMagazine07_vphvzb.jpg'
   },
   {
     name:         'The Muse Haus II - Black Sheep',
@@ -234,56 +251,18 @@ flat_attributes = [
     planet:       'Venus',
     address:      '#345-Z5G5',
     price:        9600,
-    user_id:      '6'
+    user_id:      '6',
+    remote_photo_url:        'https://res.cloudinary.com/dxzy7a44p/image/upload/v1559242983/SHC_INT_1_oct6mo.jpg'
   },
-  {
-    name:         'Bæjarins Beztu Pylsur',
-    description:  'Another journey chamber way yet females man. Way extensive and dejection get delivered deficient sincerity gentleman age. Too end instrument possession contrasted motionless. Calling offence six joy feeling. Coming merits and was talent enough far. Sir joy northward sportsmen education. Discovery incommode earnestly no he commanded if. Put still any about manor heard.',
-    planet:       'Earth',
-    address:      '#345-Z5G5',
-    price:        7000,
-    user_id:      '6'
-  },
-  {
-    name:         'Artistic Structure - Amazing View',
-    description:  'Another journey chamber way yet females man. Way extensive and dejection get delivered deficient sincerity gentleman age. Too end instrument possession contrasted motionless. Calling offence six joy feeling. Coming merits and was talent enough far. Sir joy northward sportsmen education. Discovery incommode earnestly no he commanded if. Put still any about manor heard.',
-    planet:       'Mars',
-    address:      '#345-Z5G5',
-    price:        5000,
-    user_id:      '2'
-  },
-  {
-    name:         'Stroll to Harbor from a Cozy Apartment',
-    description:  'Another journey chamber way yet females man. Way extensive and dejection get delivered deficient sincerity gentleman age. Too end instrument possession contrasted motionless. Calling offence six joy feeling. Coming merits and was talent enough far. Sir joy northward sportsmen education. Discovery incommode earnestly no he commanded if. Put still any about manor heard.',
-    planet:       'Ceres',
-    address:      '#345-Z5G5',
-    price:        5600,
-    user_id:      '4'
-  },
-  {
-    name:         'The Muse Haus I - Bæjarins Beztu Pylsur',
-    description:  'Cultivated who resolution connection motionless did occasional. Journey promise if it colonel. Can all mirth abode nor hills added. Them men does for body pure. Far end not horses remain sister. Mr parish is to he answer roused piqued afford sussex. It abode words began enjoy years no do ﻿no. Tried spoil as heart visit blush or. Boy possible blessing sensible set but margaret interest. Off tears are day blind smile alone had.',
-    planet:       'Ceres',
-    address:      '#345-Z5G5',
-    price:        7200,
-    user_id:      '4'
-  },
-  {
-    name:         'Golden Circle Glaciar',
-    description:  'Cultivated who resolution connection motionless did occasional. Journey promise if it colonel. Can all mirth abode nor hills added. Them men does for body pure. Far end not horses remain sister. Mr parish is to he answer roused piqued afford sussex. It abode words began enjoy years no do ﻿no. Tried spoil as heart visit blush or. Boy possible blessing sensible set but margaret interest. Off tears are day blind smile alone had.',
-    planet:       'Mars',
-    address:      '#345-Z5G5',
-    price:        4250,
-    user_id:      '3'
-  },
-  {
-    name:         'Stroll to Harbor from a Cozy House',
-    description:  'Cultivated who resolution connection motionless did occasional. Journey promise if it colonel. Can all mirth abode nor hills added. Them men does for body pure. Far end not horses remain sister. Mr parish is to he answer roused piqued afford sussex. It abode words began enjoy years no do ﻿no. Tried spoil as heart visit blush or. Boy possible blessing sensible set but margaret interest. Off tears are day blind smile alone had.',
-    planet:       'Venus',
-    address:      '#345-Z5G5',
-    price:        9600,
-    user_id:      '1'
-  },
+  # {
+  #   name:         'Bæjarins Beztu Pylsur',
+  #   description:  'Another journey chamber way yet females man. Way extensive and dejection get delivered deficient sincerity gentleman age. Too end instrument possession contrasted motionless. Calling offence six joy feeling. Coming merits and was talent enough far. Sir joy northward sportsmen education. Discovery incommode earnestly no he commanded if. Put still any about manor heard.',
+  #   planet:       'Earth',
+  #   address:      '#345-Z5G5',
+  #   price:        7000,
+  #   user_id:      '6',
+  #   remote_photo_url:        'image/upload/v1559242982/Future-Space-Technology-Traveling-Colonization_p409jj.jpg'
+  # }
 ]
 
 Flat.create!(flat_attributes)
